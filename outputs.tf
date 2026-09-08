acbed595bde77d253c866acb73c111fa94ee0e57
mamaducamara1 <94554360+mamaducamara1@users.noreply.github.com>
2026-09-08 12:31:51 +0100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip" {
-  description = "The public IP address of the VM instance"
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
-}
